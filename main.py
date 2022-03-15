0e4231f1382ea1f581454f2404e535a5174262fc
Lizeth <mendezlizeth084@gmail.com>
2022-03-15 15:23:22 -0700

    created conditionals set to help pick random(destination, transportation, entertainment, and restaurant)

--- main.py
+++ main.py
@@ -28,7 +28,124 @@ def randomizer(my_list):
     random_item= random.choice(my_list)
     return random_item
 
+random_destination= randomizer(destinations)
+print(f'We have selected {random_destination} for you!')
+users_destination_confirmation= input(f'Would you like to continue y or n? ')
+if users_destination_confirmation == 'y':
+    print('Awesome!')
+elif users_destination_confirmation =='n':
+    while users_destination_confirmation != 'y':
+        random_destination= randomizer(destinations)
+        users_destination_confirmation= input(f'We have selected {random_destination}, would you like to continue, y/n? ')
+print(f'{random_destination} is a great choice, now lets pick mode of transportation.')
+    
+    
+random_travel= randomizer(transportaions)
+print(f'We have selected {random_travel}.')
+users_travel_confirmation= input(f'Would you like to continue y or n? ')
+if users_travel_confirmation == 'y':
+    print('Alright,')
+elif users_travel_confirmation =='n':
+    while users_travel_confirmation != 'y':
+        random_travel= randomizer(transportaions)
+        users_travel_confirmation= input(f'We have selected {random_travel}, would you like to continue, y/n? ')
+print(f'You will get to {random_destination} by {random_travel}.')
+print('Next, we will choose the entertainment.')
 
+if random_destination == destinations[0]:
+    random_entertainment= randomizer(cali_entertainments[0])
+    print(f'We have selected {random_entertainment}.')
+    users_entertainment_confirmation= input(f'Would you like to continue y or n? ')
+    if users_entertainment_confirmation == 'y':
+        print('Ooo,fun.')
+    elif users_entertainment_confirmation =='n':
+        while users_entertainment_confirmation != 'y':
+            random_entertainment= randomizer(cali_entertainments[0])
+            users_entertainment_confirmation= input(f'We have selected {random_entertainment}, would you like to continue, y/n? ')
+if random_destination == destinations[1]:
+    random_entertainment= randomizer(cali_entertainments[1])
+    print(f'We have selected {random_entertainment}.')
+    users_entertainment_confirmation= input(f'Would you like to continue y or n? ')
+    if users_entertainment_confirmation == 'y':
+        print('Ooo,fun.')
+    elif users_entertainment_confirmation =='n':
+        while users_entertainment_confirmation != 'y':
+            random_entertainment= randomizer(cali_entertainments[1])
+            users_entertainment_confirmation= input(f'We have selected {random_entertainment}, would you like to continue, y/n? ')
+if random_destination == destinations[2]:
+    random_entertainment= randomizer(cali_entertainments[2])
+    print(f'We have selected {random_entertainment}.')
+    users_entertainment_confirmation= input(f'Would you like to continue y or n? ')
+    if users_entertainment_confirmation == 'y':
+        print('Ooo,fun.')
+    elif users_entertainment_confirmation =='n':
+        while users_entertainment_confirmation != 'y':
+            random_entertainment= randomizer(cali_entertainments[2])
+            users_entertainment_confirmation= input(f'We have selected {random_entertainment}, would you like to continue, y/n? ')
+if random_destination == destinations[3]:
+    random_entertainment= randomizer(cali_entertainments[3])
+    print(f'We have selected {random_entertainment}.')
+    users_entertainment_confirmation= input(f'Would you like to continue y or n? ')
+    if users_entertainment_confirmation == 'y':
+        print('Ooo,fun.')
+    elif users_entertainment_confirmation =='n':
+        while users_entertainment_confirmation != 'y':
+            random_entertainment= randomizer(cali_entertainments[3])
+            users_entertainment_confirmation= input(f'We have selected {random_entertainment}, would you like to continue, y/n? ')
+print('Finally, we will choose a restaurant.')
+
+if random_destination == destinations[0]:
+    random_restaurant= randomizer(cali_restaurants[0])
+    print(f'We have selected {random_restaurant}.')
+    users_restaurant_confirmation= input(f'Would you like to continue y or n? ')
+    if users_restaurant_confirmation == 'y':
+        print('Yummy!')
+    elif users_restaurant_confirmation =='n':
+        while users_restaurant_confirmation != 'y':
+            random_restaurant= randomizer(cali_restaurants[0])
+            users_restaurant_confirmation= input(f'We have selected {random_restaurant}, would you like to continue, y/n? ')
+if random_destination == destinations[1]:
+    random_restaurant= randomizer(cali_restaurants[1])
+    print(f'We have selected {random_restaurant}.')
+    users_restaurant_confirmation= input(f'Would you like to continue y or n? ')
+    if users_restaurant_confirmation == 'y':
+        print('Delicious!')
+    elif users_restaurant_confirmation =='n':
+        while users_restaurant_confirmation != 'y':
+            random_restaurant= randomizer(cali_restaurants[1])
+            users_restaurant_confirmation= input(f'We have selected {random_restaurant}, would you like to continue, y/n? ')
+if random_destination == destinations[2]:
+    random_restaurant= randomizer(cali_restaurants[2])
+    print(f'We have selected {random_restaurant}.')
+    users_restaurant_confirmation= input(f'Would you like to continue y or n? ')
+    if users_restaurant_confirmation == 'y':
+        print('Sounds tasty!')
+    elif users_restaurant_confirmation =='n':
+        while users_restaurant_confirmation != 'y':
+            random_restaurant= randomizer(cali_restaurants[2])
+            users_restaurant_confirmation= input(f'We have selected {random_restaurant}, would you like to continue, y/n? ')
+if random_destination == destinations[3]:
+    random_restaurant= randomizer(cali_restaurants[3])
+    print(f'We have selected {random_restaurant}.')
+    users_restaurant_confirmation= input(f'Would you like to continue y or n? ')
+    if users_restaurant_confirmation == 'y':
+        print('Bomb!')
+    elif users_restaurant_confirmation =='n':
+        while users_restaurant_confirmation != 'y':
+            random_restaurant= randomizer(cali_restaurants[3])
+            users_restaurant_confirmation= input(f'We have selected {random_restaurant}, would you like to continue, y/n? ')
+print(f'You will be traveling to {random_destination} by {random_travel}, exploring {random_entertainment} and ending the day with a delicious meal at {random_restaurant}.')
+
+users_trip_confirmation= input(f'Would you like to confirm your trip y or n? ')
+if users_trip_confirmation == 'y':
+        print('Sounds like a fun filled day!')
+elif users_trip_confirmation =='n':
+    while users_trip_confirmation != 'y':
+        random_destination= randomizer(destinations)
+        random_travel= randomizer(transportaions)
+        random_entertainment= randomizer(cali_entertainments)
+        random_restaurant= randomizer(cali_restaurants)
+print('Sounds like a fun filled day!')    
 
 
 
